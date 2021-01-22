fff5f46811dc1aaf10b8b5c46d84a796a7e723f9
Nick Kinnaird <nickkinn@bu.edu>
2021-01-21 22:06:46 -0600

    Final work on project, getting correlation map and county covid map.

--- code/plotUtils.py
+++ code/plotUtils.py
@@ -10,9 +10,9 @@ def makeMainResidualPlot(prediction, y_residuals, saveplot=False):
     
     plt.subplot(1, 2, 1)
     plt.scatter(prediction, y_residuals)
-    plt.title("Residual plot")
-    plt.xlabel("prediction")
-    plt.ylabel("residuals")
+    plt.title("Residuals vs Predicted")
+    plt.xlabel("Predicted Deaths per 100k")
+    plt.ylabel("Predicted - Actual Deaths per 100k")
     
     # Generates a probability plot of sample data against the quantiles of a 
     plt.subplot(1, 2, 2)
@@ -23,7 +23,6 @@ def makeMainResidualPlot(prediction, y_residuals, saveplot=False):
         plt.savefig("Images/FinalResidualsPlot.png")
     
 def makeFeatureResidualPlots(X, y_residuals, input_features):
-#     plt.figure()
     
     for col in range(X.shape[1]):
         plt.figure()
@@ -32,3 +31,17 @@ def makeFeatureResidualPlots(X, y_residuals, input_features):
         plt.xlabel(input_features[col])
         plt.ylabel("residuals")
     
+def makeComparePlot(y, y_pred, saveplot=False):
+    plt.figure(figsize=(7,7))
+    
+    plt.scatter(y_pred, y)
+    plt.xlabel("Actual Deaths per 100k")
+    plt.ylabel("Predicted Deaths per 100k")
+    
+    xpoints = ypoints = plt.xlim()
+    plt.plot(xpoints, ypoints, linestyle='--', color='r', lw=3, scalex=False, scaley=False)
+    
+    plt.title('Predicted Target vs Actual Target')
+    
+    if(saveplot):
+        plt.savefig("Images/FinalComparePlot.png")
